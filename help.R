edda4755de9efd69007bba50c91babc5652a3b40
aliiciaho <e0970625@u.nus.edu>
2024-04-07 18:03:12 +0800

    interim

---
+++ help.R
@@ -0,0 +1,75 @@
+new_data <- read.csv("/Users/aliciaho/Documents/GitHub/aliiciaho.github.io/personal_app/refined_NMdata.csv")
+
+# Cleaning data (omitting regions)
+spaces_indices <- which(trimws(new_data$Code) == "")
+cleaned_data <- new_data[-spaces_indices, ] %>% 
+  filter(Year >= 1990)
+cleaned_data$Code
+
+# Cleaning table data
+matching_values <- cleaned_data$Code %in% countries$id 
+matched_dataset1 <- cleaned_data[matching_values, ]
+print(matched_dataset1)
+unique(matched_dataset1$Code)
+
+#Finding what countries will have NA values (what is -99)
+matching_values2 <- countries$id %in% cleaned_data$Code
+countries$id[matching_values2 == FALSE]
+
+#map data
+map_data <- matched_dataset1 %>% filter(Year == 2017) %>% 
+  arrange(Entity) %>%
+  rename(index = index,
+         Entity = Entity,
+         Code = Code,
+         Year = Year,
+         Schizophrenia = Schizophrenia....,
+         Bipolar_Disorder = Bipolar.disorder....,
+         Eating_Disorder = Eating.disorders....,
+         Anxiety_Disorder = Anxiety.disorders....,
+         Druguse_Disorder = Drug.use.disorders....,
+         Depression = Depression....,
+         Alcoholuse_Disorder = Alcohol.use.disorders....)
+map_data
+
+#singapore data graph (test)
+newsgp_data <- data %>% select(-c(index, Entity)) %>%
+  filter(Code == "SGP", Year >=1990, Eating.disorders.... <100) %>%
+  pivot_longer(cols = Schizophrenia....:Alcohol.use.disorders...., names_to = "disorder_type", values_to = "percentage") %>%
+  select(Year, disorder_type, percentage) %>%
+  na.omit()
+print(ggplot(newsgp_data) +
+        aes(x = Year, y = percentage, color=disorder_type) +
+        geom_point() +
+        theme_bw() +
+        theme(axis.text.x = element_text(colour = "grey20", size = 10, angle = 90, hjust = 0.5, vjust = 0.5) 
+        ) +
+        labs(title = "Overall trend of mental health disorders in Singapore from 1990-2017", x = "Year", y = "Percentage of Singapore population") +
+        scale_color_discrete(
+          name = "Type of mental health\ndisorder",
+          labels = c("Alcohol use disorder", "Anxiety disorder", "Bipolar disorder", "Depression", "Drug use disorder", "Eating disorder", "Schizophrenia")
+        )
+)
+
+#Initial tests
+```{r eval=TRUE, include=FALSE}
+library(tidyverse)
+data <- read.csv("/Users/aliciaho/Desktop/nus yst/NM2207/NM2207 Project/Mental health Depression disorder Data.csv")
+small_data <- data %>% select(-c(index, Code))
+```
+
+Plot showing the trend of depression in Singapore from 1990-2017
+
+```{r eval = TRUE, echo=FALSE}
+sgp_data <- data %>% select(-c(index, Entity)) %>%
+  filter(Code == "SGP", Year >=1990) %>%
+  select(Year, Depression....) %>%
+  na.omit()
+ggplot(sgp_data) +
+  aes(x = Year, y = Depression....) + 
+  geom_point()+
+  labs(x = "Year", y = "Percentage of population experiencing depression", title = "Trend of depression rates in Singapore from 1990-2017") +
+  theme_bw() +
+  theme(axis.text.x = element_text(colour = "grey20", size = 10, angle = 90, hjust = 0.5, vjust = 0.5)
+  )
+```
--- personal_app/app.R
+++ personal_app/app.R
@@ -1,12 +1,26 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
+library(geojsonio)
 
-data <- read.csv("Mental health Depression disorder Data copy.csv")
-names <- unique(data$Entity)
-data$Schizophrenia.... <- as.numeric(data$Schizophrenia....)
-data$Bipolar.disorder....<- as.numeric(data$Bipolar.disorder....)
-data$Eating.disorders.... <- as.numeric(data$Eating.disorders....)
+new_data <- read.csv("refined_NMdata.csv")
+countries <- geojsonio::geojson_read("https://raw.githubusercontent.com/lvictory/maps-data/master/world.geo.json", 
+                                     what = "sp")
+
+# Cleaning data (omitting regions)
+spaces_indices <- which(trimws(new_data$Code) == "")
+cleaned_data <- new_data[-spaces_indices, ] %>% 
+  filter(Year >= 1990)
+cleaned_data$Code
+
+# Cleaning table data
+matching_values <- cleaned_data$Code %in% countries$id 
+matched_dataset1 <- cleaned_data[matching_values, ]
+
+names <- unique(matched_dataset1$Entity)
+matched_dataset1$Schizophrenia.... <- as.numeric(matched_dataset1$Schizophrenia....)
+matched_dataset1$Bipolar.disorder....<- as.numeric(matched_dataset1$Bipolar.disorder....)
+matched_dataset1$Eating.disorders.... <- as.numeric(matched_dataset1$Eating.disorders....)
 
 ui <- fluidPage(
   titlePanel("Mental Health Disorders Worldwide"),
@@ -25,28 +39,30 @@ server <- function(input, output, session){
   updateSelectizeInput(session, 'dataset', choices = names, server = TRUE)
   
   datasetInput <- eventReactive(input$dataset, {
-    data %>% filter(Entity == input$dataset)
+    matched_dataset1 %>% filter(Entity == input$dataset)
   })
   
   # Generate plot
     output$plot <- renderPlot({
       dataset <- datasetInput()
-      new_data <- dataset %>%
+      app_data <- dataset %>%
         filter(Year >=1990, Eating.disorders.... <100) %>%
         pivot_longer(cols = Schizophrenia....:Alcohol.use.disorders...., names_to = "disorder_type", values_to = "percentage") %>%
         select(Year, disorder_type, percentage) %>%
         na.omit()
-      ggplot(new_data) +
-              aes(x = Year, y = percentage, color=disorder_type) +
+      ggplot(app_data) +
+              aes(x = Year, y = percentage, group = disorder_type, color=disorder_type) +
               geom_point() +
+        geom_line() +
               theme_bw() +
               theme(axis.text.x = element_text(colour = "grey20", size = 10, angle = 90, hjust = 0.5, vjust = 0.5) 
-              ) +
+              ) + 
               labs(title = paste("Overall trend of mental health disorders in", input$dataset, "from 1990-2017"), x = "Year", y = paste("Percentage of", input$dataset, "population")) +
               scale_color_discrete(
                 name = "Type of mental health\ndisorder",
                 labels = c("Alcohol use disorder", "Anxiety disorder", "Bipolar disorder", "Depression", "Drug use disorder", "Eating disorder", "Schizophrenia")
-              )
+              ) +
+        scale_y_continuous(minor_breaks = seq(1, 5, 0.2))
     })
   }
 
